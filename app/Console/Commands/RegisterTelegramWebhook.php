837d1b34b2831bdc04aee80d5e09ba87b64a8dac
Amir Karimi <karimi.amir.dev@gmail.com>
2025-03-02 02:09:13 +0330

    [fix] fix command

--- app/Console/Commands/RegisterTelegramWebhook.php
+++ app/Console/Commands/RegisterTelegramWebhook.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace AppConsoleCommands;
+namespace App\Console\Commands;
 
-use IlluminateConsoleCommand;
-use IlluminateSupportFacadesHttp;
-use IlluminateHttpClientConnectionException;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Http\Client\ConnectionException;
 
 class RegisterTelegramWebhook extends Command
 {
